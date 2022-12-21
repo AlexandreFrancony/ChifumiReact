04be92bb6cc517f7ede82e8d8a048f074c1db8a2
Alexandre <alexandre.morel@edu.devinci.fr>
2022-12-21 16:43:09 +0100

    Maj architechture folder

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Route, Routes } from "react-router-dom";
+// import Login from './pages/login/Login';
+// import Register from './pages/Register/Register';
+// import PartyList from './pages/PartyList/PartyList';
+// import PartyDetails from './pages/PartyDetails/PartyDetails';
+
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="/" element={<Login />} ></Route>
+      <Route path="/register" element={<Register />} ></Route>
+      <Route path="/partylist" element={<PartyList />} ></Route>
+      <Route path="/partydetails" element={<PartyDetails />} ></Route> */}
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/components/Login/LoginCard/LoginCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.css';
+
+function LoginCard() {
+  return (
+    <div>LoginCard</div>
+  )
+}
+
+export default LoginCard
