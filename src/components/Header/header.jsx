c82718464283003efc1b41e85358f7b55f8413b7
AlexandreFrancony <alexandre.francony@edu.devinci.fr>
2022-12-21 16:37:54 +0100

    get out of the folder!

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function header() {
+  return (
+    <div className="header">
+        <h1>Chi-Fou-Mi</h1>
+        //TODO: Add a link to the login page
+        //TODO: Add a form to register and a button to submit
+        //TODO: Register on http://fauques.freeboxos.fr:3000/register
+    </div>
+  )
+}
+
+export default header
---
+++ src/components/RegisterCard/RegisterCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RegisterCard() {
+  return (
+    <div>RegisterCard</div>
+  )
+}
+
+export default RegisterCard
