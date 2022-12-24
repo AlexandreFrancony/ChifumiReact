4c6b7169c581c1629bab3580774cf942cf6b1b10
AlexandreFrancony <alexandre.francony@edu.devinci.fr>
2022-12-24 14:24:42 +0100

    ID match + moves

--- src/components/InGame/InGameTiles.js
+++ src/components/InGame/InGameTiles.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { useState} from 'react';
+import {useState, useEffect} from 'react';
 import './styles.css';
+import { Button } from '@mui/material';
 
 export default function InGameTiles() {
 
@@ -8,49 +9,97 @@ export default function InGameTiles() {
     const [username2, setUsername2] = useState("");
     const [matchid, setMatchid] = useState("");
     const [turnid, setTurnid] = useState("");
+    const [choice, setChoice] = useState("");
 
     /*setUsername1(localStorage.getItem('username1'));
     setUsername2(localStorage.getItem('username2'));
     setMatchid(localStorage.getItem('matchid'));
     setTurnid(0);*/
 
-    function HandleClick() {
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ matchid: matchid, username: username1, choice: 'pierre'})
-        };
-        fetch('http://fauques.freeboxos.fr:3000/matches/'+matchid+'/turns/'+turnid, requestOptions)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                (localStorage.setItem('turnid', data.idTurn))
-            });
+    //fonction qui permet de récupérer l'idturn de la partie dans l'url selon cet exemple : http://localhost:3000/matches/:idmatch/turns/:idTurn
+    function getTurnId() {
+        let url = window.location.href;
+        let id = url.substring(url.lastIndexOf('/') + 1);
+        console.log('turnid:'+id);
+        return id;
     }
 
+    //fonction qui permet de récupérer l'idmatch(int) de la partie dans l'url selon cet exemple : http://localhost:3000/matches/:idmatch/turns/:idTurn
+    function getMatchId() {
+        let url = window.location.href;
+        let id = url.substring(url.lastIndexOf('/') - 1, url.lastIndexOf('/'));
+        console.log('matchid:'+id);
+        return id;
+    }
+
+    useEffect(() => {
+        setMatchid(getMatchId());
+        setTurnid(getTurnId());
+    }, []) 
+
+    function HandleClick (e) {
+        switch (e) {
+            case "rock":
+                setChoice('rock');
+                break;
+            case "paper":
+                setChoice('paper');
+                break;
+            case "scissors":
+                setChoice('scissors');
+                break;
+            default:
+                console.log("error in switch handleChange");
+                break;
+        }
+    }
+
+    function Request() { 
+        const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({'move': choice})
+    };
+    fetch('http://fauques.freeboxos.fr:3000/matches/'+matchid+'/turns/'+turnid, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setTurnid(data.turnid)
+        });
+        console.log(requestOptions)
+    }
+
+    useEffect(() => {
+        if (choice) {
+            Request()
+        }
+    }, [choice])
 
     return(
         <div className="Wrapper">
             <h2>{username1 + ' vs ' + username2}</h2>
             <h3>{'ID of the current match : ' + matchid}</h3>
             <div className="tiles">
-                <div className="Card" onClick={HandleClick}>
-                    <h3>Pierre</h3>
+                <div className="Card">
+                    <h3 className='icons'>Rock</h3>
                     <div className='icons'>
                         👊
                     </div>
+                    <Button variant="text" onClick={(e) => HandleClick('rock')}>Select</Button>
                 </div>
                 <div className="Card">
-                    <h3>Feuille</h3>
+                    <h3 className='icons'>Paper</h3>
                     <div className='icons'>
                         🖐
                     </div>
+                    <Button variant="text" onClick={(e) => HandleClick('paper')}>Select</Button>
                 </div>
                 <div className="Card">
-                    <h3>Ciseau</h3>
+                    <h3 className='icons'>Scissors</h3>
                     <div className='icons'>
                         ✌️
                     </div>
+                    <Button variant="text" onClick={(e) => HandleClick('scissors')}>Select</Button>
                 </div>
             </div>
         </div>
